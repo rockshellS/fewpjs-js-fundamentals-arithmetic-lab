fb599e91a1a5522310d136d8994d25db0b4d6685
rockshellS <spicer.rachelle@gmail.com>
2021-02-15 21:53:24 -0600

    commit tree dc1d5ab273f94635d939e7ce6f74f889d718b044

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 8) + 1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max( 5, 10, 15, 20 );
